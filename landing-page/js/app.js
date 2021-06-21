ab9fdff4513bd805d24d2b786583d81614a5d54e
ledia8 <32582491+ledia8@users.noreply.github.com>
2021-06-21 19:25:32 +0200

    Add files via upload

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -35,12 +35,10 @@
 */
 
 // build the nav
-//function create elements in nav bar
+//createNavElement function create elements in nav bar
 //like this <li><a href="section1">Section1</a></li>
-// console.log("I am in js");//only for test code
 
 function createNavElement(textElement){
-    // console.log("I am in function");//only for test code
     let ul = document.getElementById('navbar__list');
     let li = document.createElement('li');//li --> list
     let a = document.createElement('a');// a --> ancor
@@ -52,16 +50,12 @@ function createNavElement(textElement){
     a.addEventListener("click", ()=> {
     //when clicked on anchor,it go to the current section 
     goToRightSection(currentSection)
+
     //the next two lines change the color of the section word in nave bar 
     //and the section in page when click on section in nave bar
     a.className = "active_class";
     currentSection.className = "active_class";
 });
-// a.classList.remove("active_class");
-// var c = a.getAttribute("class");
-//     console.log("c---  " + c);
-
-    console.log(ul);//only for test code
 }
 
 //this loop for create five element in nav bar
@@ -72,37 +66,17 @@ for(var i=0;i<sections.length;i++){
     sectionID = sections[i].getAttribute("id");
     createNavElement(sectionID);  //to create all sections
 }
-//  //getActiveSection using getBoundingClientRect function
-// for(var i=0;i<sections.length;i++){
-//     sectionID = sections[i].getAttribute("id");
-//     let sec = document.getElementById(sectionID);
-//     // var top = getActiveSection(sec);
-//     //------------------ to get the current section -------------- 
-//     let top = sections[i].getBoundingClientRect().top;
-//     if (top>0 && top<300){
-//         sec.classList.add("your-active-class");
-//     }else if (sections[i].classList.contains("your-active-class")){
-//         sections[i].classList.remove("your-active-class");
-//     }else{
-//         continue;
-//     }
-// }
 
+//-------- getActiveSection using getBoundingClientRect function -----------
 function activeSection(){
-//getActiveSection using getBoundingClientRect function
-
 for(var i=0;i<sections.length;i++){
 
     sectionID = sections[i].getAttribute("id");
     
-    // let sec = document.getElementById(sectionID);
-    
-    
     //------------------ to get the current section -------------- 
-    
     let top = sections[i].getBoundingClientRect().top;
     
-    if (top>0 && top<300){
+    if (top>-50 && top<300){
         sections[i].classList.add("your-active-class");
     
     }else if (sections[i].classList.contains("your-active-class")){
@@ -115,7 +89,8 @@ for(var i=0;i<sections.length;i++){
 window.addEventListener("scroll", activeSection);
 
 
-// this function will scrolling smoothly to the section required --> it's parameter the section getting by Id
+// this function will scrolling smoothly to the section required -->
+// it's parameter the section getting by Id
 function goToRightSection(sectionRequire){
     sectionRequire.scrollIntoView({behavior: "smooth"});
 }
